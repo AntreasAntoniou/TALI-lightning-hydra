e709c46ac3762998dc054515f9b4c81c653518b0
AntreasAntoniou <a.antoniou@ed.ac.uk>
2022-01-20 15:14:41 +0000

    Base experiment working.

--- setup_scripts/startup_script.sh
+++ setup_scripts/startup_script.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-cd $HOME
-CONDA_DIR=$HOME/conda/;
 source $CONDA_DIR/bin/activate;
 conda activate tali;
 
-cd $CODE_DIR;
-git pull
 bash $CODE_DIR/setup_scripts/setup_base_experiment_disk.sh
 bash $CODE_DIR/setup_scripts/setup_tali_dataset_disk.sh
 wandb agent evolvingfungus/TALI-gcp-sweep-1/5ls9k9ch
