7be511e18e25cc94a871cfb9d21f579ae7e7d2ea
AntreasAntoniou <a.antoniou@ed.ac.uk>
2022-01-23 23:34:17 +0000

    Base experiment working.

--- tali/run_data_only.py
+++ tali/run_data_only.py
@@ -4,15 +4,32 @@ import hydra
 import torch
 import tqdm
 from omegaconf import DictConfig
+import matplotlib.pyplot as plt
 from pytorch_lightning import seed_everything, LightningDataModule
+from torchvision.utils import make_grid
+import numpy as np
 
 from tali.utils.storage import pretty_print_dict
 
 log = logging.getLogger(__name__)
 
 
-def sample_datamodule(config: DictConfig):
+def plot_with_spectrum(x, rate=48000):
+    """Plot the given waveform (timeSeries), both as time-domain and as its
+    frequency-domain spectrum. Returns a matplotlib.figure.Figure object."""
+    fig, axs = plt.subplots(2)
+    n = len(x)
+    # (1) Plot time-domain data:
+    timesMsec = np.arange(n) * 1000.0 / rate
+    axs[0].plot(timesMsec, x)
+    # Limit the X axis to our input samples:
+    axs[0].set_xlabel("Time (ms)")
+    axs[0].grid(True)
+    # (2) Compute and plot frequency spectrum:
+    return fig
 
+
+def sample_datamodule(config: DictConfig):
     seed_everything(config.seed, workers=True)
 
     log.info(f"{pretty_print_dict(dict(config))}")
@@ -32,6 +49,38 @@ def sample_datamodule(config: DictConfig):
                                  f'invalid count: {total_invalid},'
                                  f'percentage of invalid: '
                                  f'{total_invalid / (total_valid + total_invalid)}')
+            # log.info(item_batch)
 
-
-
+            # grid = make_grid(
+            #     torch.cat(
+            #         [
+            #             item_batch["image"],
+            #             item_batch["video"].view(
+            #                 -1,
+            #                 *item_batch["video"].shape[2:],
+            #             ),
+            #         ],
+            #         dim=0,
+            #     ),
+            #     normalize=True,
+            #     value_range=None,
+            #     scale_each=True,
+            #     pad_value=0,
+            #     nrow=30,
+            # )
+            #
+            # for item_idx, audio_item in enumerate(item_batch["audio"]):
+            #     logging.info(f'{torch.mean(audio_item), torch.std(audio_item)}, ')
+            #     # torchaudio.save(
+            #     #     f"save_example_default_{item_idx}.wav",
+            #     #     audio_item.permute([1, 0]),
+            #     #     44100,
+            #     # )
+            #     for channel in range(2):
+            #         fig = plotWithSpectrum(audio_item[channel], rate=44100)
+            #         fig.savefig(f"{config.exp_dir}/{item_idx}_{channel}.png")
+            #         plt.close()
+            #
+            # plt.imshow(grid.permute([1, 2, 0]).cpu().numpy())
+            # plt.show()
+            # input("Press Enter to continue...")
