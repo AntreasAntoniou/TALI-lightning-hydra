85525b1ca87c259a67a85289dba093d423c15383
AntreasAntoniou <a.antoniou@ed.ac.uk>
2022-01-23 22:25:15 +0000

    Base experiment working.

--- tali/run_data_only.py
+++ tali/run_data_only.py
@@ -20,9 +20,17 @@ def sample_datamodule(config: DictConfig):
 
     datamodule = hydra.utils.instantiate(config.data, _recursive_=False)
     datamodule.setup(stage="fit")
+    total_valid = 0
+    total_invalid = 0
     with tqdm.tqdm(total=len(datamodule.train_dataloader())) as pbar:
-        for item_batch in datamodule.train_dataloader():
+        for item_batch, none_count in datamodule.train_dataloader():
+            total_valid += len(item_batch)
+            total_invalid += none_count
             pbar.update(1)
+            pbar.set_description(f'valid count: {total_valid}, '
+                                 f'invalid cound {total_invalid},'
+                                 f'percentage of invalids '
+                                 f'{total_invalid / (total_valid + total_invalid)}')
 
 
 if __name__ == "__main__":
