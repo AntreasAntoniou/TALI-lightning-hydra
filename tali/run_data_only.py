2c73cd8799951010c3991a3d886f04b64e1928bf
AntreasAntoniou <a.antoniou@ed.ac.uk>
2022-01-25 15:11:03 +0000

    Base experiment working.

--- tali/run_data_only.py
+++ tali/run_data_only.py
@@ -40,15 +40,16 @@ def sample_datamodule(config: DictConfig):
     datamodule.setup(stage="fit")
     total_valid = 0
     total_invalid = 0
-    with tqdm.tqdm(total=len(datamodule.train_dataloader())) as pbar:
-        for item_batch, none_count in datamodule.train_dataloader():
-            total_valid += len(item_batch)
-            total_invalid += none_count
-            pbar.update(1)
-            pbar.set_description(f'valid count: {total_valid}, '
-                                 f'invalid count: {total_invalid},'
-                                 f'percentage of invalid: '
-                                 f'{total_invalid / (total_valid + total_invalid)}')
+    for _ in range(config.trainer.max_epochs):
+        with tqdm.tqdm(total=len(datamodule.train_dataloader())) as pbar:
+            for item_batch, none_count in datamodule.train_dataloader():
+                total_valid += len(item_batch)
+                total_invalid += none_count
+                pbar.update(1)
+                pbar.set_description(f'valid count: {total_valid}, '
+                                     f'invalid count: {total_invalid},'
+                                     f'percentage of invalid: '
+                                     f'{total_invalid / (total_valid + total_invalid)}')
             # log.info(item_batch)
 
             # grid = make_grid(
