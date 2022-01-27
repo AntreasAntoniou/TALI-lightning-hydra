6e5cdbcc1b6f1dddd452785d9a8bd1f95c26e4f5
AntreasAntoniou <a.antoniou@ed.ac.uk>
2022-01-27 23:22:42 +0000

    Base experiment working.

--- setup_scripts/startup_call.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "ALL HAIL THE SCRIPT"
-source /root/.bashrc
-cd /root/TALI-lightning-hydra/
-git pull
-bash /root/TALI-lightning-hydra/setup_scripts/startup_script.sh
-echo "STARTUP SCRIPT DONE, hopefully"
-
-#sudo -u evolvingfungus bash -c 'bash ~/startup_script.sh'
