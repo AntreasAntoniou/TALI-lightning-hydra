459f0c44c2cd4fe023f3bb20b2bdd107a0b29c9f
AntreasAntoniou <a.antoniou@ed.ac.uk>
2022-02-01 09:48:39 +0000

    Add script that enables parallel convertion of all .mp4 files into jpeg frames.

--- preprocessing_scripts/convert_videos_to_frame_jpegs.py
+++ preprocessing_scripts/convert_videos_to_frame_jpegs.py
@@ -24,6 +24,17 @@ import tqdm
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# add formatter to ch
+ch.setFormatter(formatter)
+
+# add ch to logger
+log.addHandler(ch)
 
 class VideoToFrameError(Exception):
     """Base class for exceptions in this module."""
