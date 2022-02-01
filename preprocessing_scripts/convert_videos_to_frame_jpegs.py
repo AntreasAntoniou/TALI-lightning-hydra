caed1bc1ca9de2a4eefd55d1132aac51fd135f54
AntreasAntoniou <a.antoniou@ed.ac.uk>
2022-02-01 09:45:44 +0000

    Add script that enables parallel convertion of all .mp4 files into jpeg frames.

--- preprocessing_scripts/convert_videos_to_frame_jpegs.py
+++ preprocessing_scripts/convert_videos_to_frame_jpegs.py
@@ -91,7 +91,7 @@ def delete_file_if_exists(path: pathlib.Path):
 
 if __name__ == "__main__":
     args = get_base_arguments()
-    log.info(f'START THIS')
+    log.info(f'START THIS\n\n\n')
     print('start this')
     if not os.path.exists(args.source_filepath):
         log.error(f"Source path {args.source_filepath} not found")
