e0e2c939998071a03e6b89b6d913a303f39ebcca
AntreasAntoniou <a.antoniou@ed.ac.uk>
2022-02-11 19:04:32 +0000

    Remove overhead of randomly removed samples from downstream processes when scanning filepaths.

--- tali/datasets/datasets.py
+++ tali/datasets/datasets.py
@@ -154,9 +154,22 @@ class TALIMultiModalDataset(Dataset):
                 return None
 
             if pathlib.Path(audio_filepath).with_suffix(".npy").exists():
-                frames_dict.audio = torch.Tensor(
-                    np.load(audio_filepath.replace(".aac", ".npy"), allow_pickle=True)
-                )
+                try:
+                    frames_dict.audio = torch.Tensor(
+                        np.load(
+                            audio_filepath.replace(".aac", ".npy"), allow_pickle=True
+                        )
+                    )
+                except Exception:
+                    if pathlib.Path(audio_filepath).exists():
+                        frames_dict.audio = convert_aac_to_npy(
+                            filepath=audio_filepath,
+                            delete_original=True,
+                            sample_rate=self.config.num_audio_sample_rate,
+                            mono=False,
+                            in_type=np.float32,
+                            out_type=np.float32,
+                        )
             else:
                 frames_dict.audio = convert_aac_to_npy(
                     filepath=audio_filepath,
