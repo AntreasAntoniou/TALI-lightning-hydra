e2dbecc9eb9702d72ffeddc713e6cabf81d15f31
AntreasAntoniou <a.antoniou@ed.ac.uk>
2022-03-07 01:00:25 +0000

    Debug new feature

--- tali/base/callbacks/pytorch_lightning_custom.py
+++ tali/base/callbacks/pytorch_lightning_custom.py
@@ -8,4 +8,4 @@ class RunValidationOnTrainStart(Callback):
         super().__init__()
 
     def on_train_start(self, trainer: Trainer, pl_module: LightningModule) -> None:
-        trainer.validate(model=pl_module)
+        trainer.validate(model=pl_module, val_dataloaders=pl_module.val_dataloader())
